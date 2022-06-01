435e94731d7bb3994f5c8ff47feeed3a910372ff
TelmoMorais <telmolmorais01@gmail.com>
2022-06-01 14:10:57 +0100

    Update Open Helper

--- app/src/main/java/com/example/reservarestaurantes/BDReservasRestauranteOpenHelper.kt
+++ app/src/main/java/com/example/reservarestaurantes/BDReservasRestauranteOpenHelper.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class BDLivrosOpenHelper(
+class BDReservasRestauranteOpenHelper(
     context: Context?,
     name: String?,
     factory: SQLiteDatabase.CursorFactory?,
