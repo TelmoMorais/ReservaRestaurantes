f68d56bc1cf02453de5a352722e90390820d3570
TelmoMorais <telmolmorais01@gmail.com>
2022-06-13 17:46:35 +0100

    Criadas funções para converter as classes Clientes, Mesas e Refeição em ContentValues

--- app/src/main/java/com/example/reservarestaurantes/Mesas.kt
+++ app/src/main/java/com/example/reservarestaurantes/Mesas.kt
@@ -1,4 +1,14 @@
 package com.example.reservarestaurantes
 
+import android.content.ContentValues
+
 data class Mesas (var id: Long, var numero_mesa: Long, var quantidade_lugares: Long) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDMesas.CAMPO_NUMERO_MESA, numero_mesa)
+        valores.put(TabelaBDMesas.CAMPO_QUANTIDADE_LUGARES, quantidade_lugares)
+
+        return valores
+    }
 }
--- app/src/main/java/com/example/reservarestaurantes/Refeicao.kt
+++ app/src/main/java/com/example/reservarestaurantes/Refeicao.kt
@@ -1,4 +1,13 @@
 package com.example.reservarestaurantes
 
+import android.content.ContentValues
+
 data class Refeicao (var id: Long, var tipo_refeicao: String) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDRefeicao.CAMPO_TIPO_REFEICAO, tipo_refeicao)
+
+        return valores
+    }
 }
