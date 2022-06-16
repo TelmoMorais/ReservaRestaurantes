e54e9ef5416dfa1e17c40f94edcaa76a2a4aceae
TelmoMorais <telmolmorais01@gmail.com>
2022-06-16 17:05:16 +0100

    Criada função para converter um cursor numa mesa.

--- app/src/main/java/com/example/reservarestaurantes/Mesas.kt
+++ app/src/main/java/com/example/reservarestaurantes/Mesas.kt
@@ -1,6 +1,8 @@
 package com.example.reservarestaurantes
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Mesas (var numero_mesa: Int, var quantidade_lugares: Int, var id: Long = -1 ) {
     fun toContentValues() : ContentValues {
@@ -10,5 +12,20 @@ data class Mesas (var numero_mesa: Int, var quantidade_lugares: Int, var id: Lon
         valores.put(TabelaBDMesas.CAMPO_QUANTIDADE_LUGARES, quantidade_lugares)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Mesas{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNrMesa = cursor.getColumnIndex(TabelaBDMesas.CAMPO_NUMERO_MESA)
+            val posQantLugares = cursor.getColumnIndex(TabelaBDMesas.CAMPO_QUANTIDADE_LUGARES)
+
+            val id = cursor.getLong(posId)
+            val nrMesa = cursor.getInt(posNrMesa)
+            val quantLugares = cursor.getInt(posQantLugares)
+
+
+            return Mesas(nrMesa, quantLugares, id)
+        }
     }
 }
