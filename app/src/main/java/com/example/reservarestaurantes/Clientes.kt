ef282e0d03f0c00dca8a08974b11049fa243fd60
TelmoMorais <telmolmorais01@gmail.com>
2022-06-16 17:01:46 +0100

    Criada função para converter um cursor num cliente.

--- app/src/main/java/com/example/reservarestaurantes/Clientes.kt
+++ app/src/main/java/com/example/reservarestaurantes/Clientes.kt
@@ -1,6 +1,8 @@
 package com.example.reservarestaurantes
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Clientes (var nome:String, var contato_telefonico:String, var nif:String, var morada: String, var id:Long = -1) {
 
@@ -13,6 +15,24 @@ data class Clientes (var nome:String, var contato_telefonico:String, var nif:Str
         valores.put(TabelaBDClientes.CAMPO_MORADA, morada)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Clientes{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDClientes.CAMPO_NOME)
+            val posTelefone = cursor.getColumnIndex(TabelaBDClientes.CAMPO_TELEFONE)
+            val posNif = cursor.getColumnIndex(TabelaBDClientes.CAMPO_NIF)
+            val posMorada = cursor.getColumnIndex(TabelaBDClientes.CAMPO_MORADA)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val telefone = cursor.getString(posTelefone)
+            val nif = cursor.getString(posNif)
+            val morada = cursor.getString(posMorada)
+
+            return Clientes(nome, telefone,nif, morada, id)
+        }
     }
 
 }
