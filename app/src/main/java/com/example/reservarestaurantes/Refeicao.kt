9c88750facc63e5319dbb5ae07baca9d3ab64eb6
TelmoMorais <telmolmorais01@gmail.com>
2022-06-16 17:07:08 +0100

    Criada função para converter um cursor numa refeição.

--- app/src/main/java/com/example/reservarestaurantes/Refeicao.kt
+++ app/src/main/java/com/example/reservarestaurantes/Refeicao.kt
@@ -1,6 +1,8 @@
 package com.example.reservarestaurantes
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Refeicao (var tipo_refeicao: String, var id: Long = -1) {
     fun toContentValues() : ContentValues {
@@ -9,5 +11,19 @@ data class Refeicao (var tipo_refeicao: String, var id: Long = -1) {
         valores.put(TabelaBDRefeicao.CAMPO_TIPO_REFEICAO, tipo_refeicao)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Refeicao{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posRefeicao = cursor.getColumnIndex(TabelaBDRefeicao.CAMPO_TIPO_REFEICAO)
+
+            val id = cursor.getLong(posId)
+            val tipoRefeicao = cursor.getString(posRefeicao)
+
+
+
+            return Refeicao(tipoRefeicao, id)
+        }
     }
 }
