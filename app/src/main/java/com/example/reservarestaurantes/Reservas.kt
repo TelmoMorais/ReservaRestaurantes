d8a511580d17f036ad7a32a37de9c3dc5ae4f691
TelmoMorais <telmolmorais01@gmail.com>
2022-06-17 11:01:07 +0100

    Criada função para converter um Cursor numa reserva.

--- app/src/main/java/com/example/reservarestaurantes/Reservas.kt
+++ app/src/main/java/com/example/reservarestaurantes/Reservas.kt
@@ -1,6 +1,8 @@
 package com.example.reservarestaurantes
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 import java.util.*
 
 data class Reservas (var data_reserva:Int, var numero_pessoas:Int, var clientes_id:Long, var mesas_id:Long, var refeicao_id:Long, var id:Long = -1) {
@@ -15,6 +17,27 @@ data class Reservas (var data_reserva:Int, var numero_pessoas:Int, var clientes_
         valores.put(TabelaBDReservas.CAMPO_REFEICAO_ID, refeicao_id)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Reservas{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posDataRes = cursor.getColumnIndex(TabelaBDReservas.CAMPO_DATARESERVA)
+            val posNrPess = cursor.getColumnIndex(TabelaBDReservas.CAMPO_NRPESSOAS)
+            val posClienteId = cursor.getColumnIndex(TabelaBDReservas.CAMPO_CLIENTES_ID)
+            val posMesaId = cursor.getColumnIndex(TabelaBDReservas.CAMPO_MESAS_ID)
+            val posRefeicaoId = cursor.getColumnIndex(TabelaBDReservas.CAMPO_REFEICAO_ID)
+
+            val id= cursor.getLong(posId)
+            val dataRes = cursor.getInt(posDataRes)
+            val nrPess = cursor.getInt(posNrPess)
+            val clienteId = cursor.getLong(posClienteId)
+            val mesaId = cursor.getLong(posMesaId)
+            val refeicaoId = cursor.getLong(posRefeicaoId)
+
+            return Reservas(dataRes, nrPess,clienteId,mesaId,refeicaoId, id)
+
+        }
     }
 
 }
