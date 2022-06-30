936d0f2a52d611e4012e4be6c9463b140251c00f
TelmoMorais <telmolmorais01@gmail.com>
2022-06-30 22:58:29 +0100

    Passar os dados das reservas para o adptador

--- app/src/main/java/com/example/reservarestaurantes/AdapterReservas.kt
+++ app/src/main/java/com/example/reservarestaurantes/AdapterReservas.kt
@@ -1,10 +1,20 @@
 package com.example.reservarestaurantes
 
+import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterReservas : RecyclerView.Adapter<AdapterReservas.ViewHolderReserva>() {
+
+    var cursor: Cursor? = null
+    get() = field
+    set(value){
+        if(field != value){
+            field = value
+            notifyDataSetChanged()
+        }
+    }
 
     class ViewHolderReserva(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
