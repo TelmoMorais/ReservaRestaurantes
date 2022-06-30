f3c480bbbb06210a9f804d51b89027fb4f2bef65
TelmoMorais <telmolmorais01@gmail.com>
2022-06-30 23:03:57 +0100

    Implementada função getItemCount do AdapterMesas

--- app/src/main/java/com/example/reservarestaurantes/AdapterMesas.kt
+++ app/src/main/java/com/example/reservarestaurantes/AdapterMesas.kt
@@ -78,6 +78,8 @@ class AdapterMesas : RecyclerView.Adapter<AdapterMesas.ViewHolderMesa>() {
      * @return The total number of items in this adapter.
      */
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        if (cursor == null) return 0
+
+        return cursor!!.count
     }
 }
