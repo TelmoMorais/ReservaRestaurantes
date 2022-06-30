ca6c741ac6fac3854394c955c12ad2630b61a4e2
TelmoMorais <telmolmorais01@gmail.com>
2022-06-30 11:48:15 +0100

    Criado fragmento lista de clientes

--- app/src/main/java/com/example/reservarestaurantes/ListaClientesFragment.kt
+++ app/src/main/java/com/example/reservarestaurantes/ListaClientesFragment.kt
@@ -6,29 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.reservarestaurantes.databinding.FragmentMenuPrincipalBinding
+import com.example.reservarestaurantes.databinding.FragmentListaClientesBinding
 
-class MenuPrincipalFragment : Fragment() {
-    private var _binding: FragmentMenuPrincipalBinding? = null
+class ListaClientesFragment : Fragment() {
+    private var _binding: FragmentListaClientesBinding? = null
 
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false)
+        _binding = FragmentListaClientesBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       // binding.buttonFirst.setOnClickListener {
-         //   findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+    //    binding.buttonFirst.setOnClickListener {
+      //      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         //}
     }
 
