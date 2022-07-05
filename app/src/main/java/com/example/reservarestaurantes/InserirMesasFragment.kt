7feac584685b05172157479a7de7fe8da0d666c3
TelmoMorais <telmolmorais01@gmail.com>
2022-07-05 21:30:09 +0100

    Fragmento inserir mesas modififcado

--- app/src/main/java/com/example/reservarestaurantes/InserirMesasFragment.kt
+++ app/src/main/java/com/example/reservarestaurantes/InserirMesasFragment.kt
@@ -1,59 +1,113 @@
 package com.example.reservarestaurantes
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
+import com.example.reservarestaurantes.databinding.FragmentInserirMesasBinding
+import com.example.reservarestaurantes.databinding.FragmentInserirReservaBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [InserirMesasFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class InserirMesasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class InserirMesasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
+    private var _binding: FragmentInserirMesasBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() =_binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inserir_mesas, container, false)
+        _binding = FragmentInserirMesasBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InserirMesasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InserirMesasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    }
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param id The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is *not* allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
+     *
+     *
+     * This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     *
+     *
+     *  *
+     *
+     *The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a [android.database.Cursor]
+     * and you place it in a [android.widget.CursorAdapter], use
+     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
+     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
+     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     *  *  The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a [android.database.Cursor] from a [android.content.CursorLoader],
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * [android.widget.CursorAdapter], you should use the
+     * [android.widget.CursorAdapter.swapCursor]
+     * method so that the old Cursor is not closed.
+     *
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
