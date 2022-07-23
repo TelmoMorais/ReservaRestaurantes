497c3ed0ec8c0bddc0551b9d9bf14c66005ab4a7
TelmoMorais <telmolmorais01@gmail.com>
2022-07-23 21:39:03 +0100

    Fragmentos Inserir atualizados

--- app/src/main/java/com/example/reservarestaurantes/InserirReservaFragment.kt
+++ app/src/main/java/com/example/reservarestaurantes/InserirReservaFragment.kt
@@ -18,15 +18,12 @@ import com.example.reservarestaurantes.databinding.FragmentInserirReservaBinding
 import com.google.android.material.snackbar.Snackbar
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InserirReservaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     private var _binding:FragmentInserirReservaBinding? = null
 
     private val binding get() =_binding!!
+
+    private var loader: CursorLoader? = null
 
 
 
@@ -35,7 +32,6 @@ class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentInserirReservaBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
@@ -59,8 +55,8 @@ class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
 
     companion object {
         const val ID_LOADER_CLIENTES = 0
-        const val ID_LOADER_MESAS = 0
-        const val ID_LOADER_REFEICOES = 0
+        const val ID_LOADER_MESAS = 1
+        const val ID_LOADER_REFEICOES = 2
     }
 
     /**
@@ -74,14 +70,40 @@ class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
      * @return Return a new Loader instance that is ready to start loading.
      */
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+
+
         CursorLoader(
-            requireContext(),
-            ContentProviderReservas.ENDERECO_CLIENTES,
-            TabelaBDClientes.TODOS_CAMPOS_CIENTES,
-            null,
-            null,
-            "${TabelaBDClientes.CAMPO_NOME}"
-        )
+                requireContext(),
+                ContentProviderReservas.ENDERECO_CLIENTES,
+                TabelaBDClientes.TODOS_CAMPOS_CIENTES,
+                null,
+                null,
+                "${TabelaBDClientes.CAMPO_NOME}")
+
+
+    //       if(id == ID_LOADER_CLIENTES){
+//            loader = CursorLoader(
+//                requireContext(),
+//                ContentProviderReservas.ENDERECO_CLIENTES,
+//                TabelaBDClientes.TODOS_CAMPOS_CIENTES,
+//                null,
+//                null,
+//                "${TabelaBDClientes.CAMPO_NOME}"
+//            )
+//        }  else if (id == ID_LOADER_MESAS){
+//            loader = CursorLoader(
+//                requireContext(),
+//                ContentProviderReservas.ENDERECO_MESAS,
+//                TabelaBDMesas.TODOS_CAMPOS_MESAS,
+//                null,
+//                null,
+//                "${TabelaBDMesas.CAMPO_NUMERO_MESA}"
+//            )
+//        }
+//        return loader as CursorLoader
+
+
+
 
 
     /**
@@ -136,8 +158,33 @@ class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
             intArrayOf(android.R.id.text1),
             0
         )
-
         binding.spinnerClienteReserva.adapter = adapterClientes
+//        if (loader.id == ID_LOADER_CLIENTES){
+//            val adapterClientes = SimpleCursorAdapter(
+//                requireContext(),
+//                android.R.layout.simple_list_item_1,
+//                data,
+//                arrayOf(TabelaBDClientes.CAMPO_NOME),
+//                intArrayOf(android.R.id.text1),
+//                0
+//            )
+//            binding.spinnerClienteReserva.adapter = adapterClientes
+//
+//        }else if (loader.id == ID_LOADER_MESAS){
+//            val adapterMesas = SimpleCursorAdapter(
+//                requireContext(),
+//                android.R.layout.simple_list_item_1,
+//                data,
+//                arrayOf(TabelaBDMesas.CAMPO_NUMERO_MESA),
+//                intArrayOf(android.R.id.text1),
+//                0
+//            )
+//
+//            binding.spinnerMesaReserva.adapter = adapterMesas
+//        }
+
+
+
     }
 
     /**
@@ -151,8 +198,9 @@ class InserirReservaFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
      * @param loader The Loader that is being reset.
      */
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        if (_binding == null) return
+
         binding.spinnerClienteReserva.adapter = null
+        binding.spinnerMesaReserva.adapter = null
     }
 
     fun processaOpcaoMenu(item: MenuItem) : Boolean =
